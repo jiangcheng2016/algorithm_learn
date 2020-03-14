0c58625ed1d651808e47bb051452fbd00a4e4c28
jiangsir <jianglvforever@163.com>
2020-03-14 17:42:02 +0800

    求和和次数

---
+++ acwing/84.求1+2+3+...+n.c
@@ -0,0 +1,17 @@
+/*
+	求1+2+…+n,要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+	样例
+	输入：10
+
+	输出：55
+*/
+
+class Solution {
+public:
+    int getSum(int n) {
+        int res = n;
+        n > 0 && (res += getSum(n - 1));
+        return res;
+    }
+};

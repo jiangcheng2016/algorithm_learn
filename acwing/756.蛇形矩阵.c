057bf6e517846fe2838e60a2433aec586b6b8294
jiangsir <jianglvforever@163.com>
2020-03-16 17:28:49 +0800

    蛇形矩阵

---
+++ acwing/756.蛇形矩阵.c
@@ -0,0 +1,64 @@
+/*
+	
+	输入两个整数n和m，输出一个n行m列的矩阵，将数字 1 到 n*m 按照回字蛇形填充至矩阵中。
+
+	具体矩阵形式可参考样例。
+
+	输入格式
+	输入共一行，包含两个整数n和m。
+
+	输出格式
+	输出满足要求的矩阵。
+
+	矩阵占n行，每行包含m个空格隔开的整数。
+
+	数据范围
+	1≤n,m≤100
+	输入样例：
+	3 3
+	输出样例：
+	1 2 3
+	8 9 4
+	7 6 5
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int N = 110;
+
+int n, m;
+int res[N][N];
+
+int main()
+{
+    cin >> n >> m;
+    
+    int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+    
+    for (int x = 0, y = 0, d = 0, k = 1; k <= n * m; k ++)
+    {
+        res[x][y] = k;
+        int a = x + dx[d], b = y + dy[d];
+        if(a < 0 || a >= n || b < 0 || b >= m || res[a][b] )
+        {
+            d = (d + 1) % 4;
+            a = x + dx[d], b = y + dy[d];
+        }
+        x = a, y = b;
+        
+    }
+    
+    for (int i = 0;i < n; i ++)
+    {
+        for (int j = 0; j < m; j ++)
+            cout << res[i][j] << ' ';
+        cout << endl;
+    }
+    
+    return 0;
+    
+}

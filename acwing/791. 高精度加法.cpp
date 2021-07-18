eaf6ea85bd77f096bf078ee1eacc2927d7e6fdbc
jiangcheng2016 <jiangchengc2016@163.com>
2021-07-18 22:16:08 +0800

    lc88

--- acwing/791. 高精度加法.cpp
+++ acwing/791. 高精度加法.cpp
@@ -59,3 +59,25 @@ int main()
 
 }
 
+/**
+ * java
+ */
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String args[]) throws Exception{
+       // Scanner sc = new Scanner(System.in);
+       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        //BigInteger a = sc.nextBigInteger();
+        //BigInteger b = sc.nextBigInteger();
+        BigInteger a = new BigInteger(reader.readLine());
+        BigInteger b = new BigInteger(reader.readLine());
+        
+        System.out.println(a.add(b));
+        reader.close();
+    }
+}
